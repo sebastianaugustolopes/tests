9c9a4702f69b58581f9b92ebbc6fd03774959251
sebastianaugustolopes <sebastianaugustolopes@gmail.com>
2024-10-20 20:03:32 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let toggleButton = document.getElementById('toggle-btn')
+let sidebar = document.getElementById('sidebar')
+
+function toggleSidebar(){
+    sidebar.classList.toggle('close')
+    toggleButton.classList.toggle('rotate')
+
+    Array.from(sidebar.getElementsByClassName('show')).forEach(ul =>{
+        ul.classList.remove('show')
+        ul.previousElementSibling.classList.remove('rotate')
+    })
+}
+
+function toggleSubMenu(button){
+    button.nextElementSibling.classList.toggle('show')
+    button.classList.toggle('rotate')
+
+    if(sidebar.classList.contains('close')){
+        sidebar.classList.toggle('close')
+        toggleButton.classList.toggle('rotate')
+    }
+}
